0924d3077cec0a3a53146181a42e3a3367c80930
duda0812 <mariaeduarda.afs08@gmail.com>
2025-11-17 13:17:14 -0400

    teste

---
+++ lib/pontosColeta.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pontos extends StatefulWidget {
+  const Pontos({super.key});
+
+  @override
+  State<Pontos> createState() => _PontosState();
+}
+
+class _PontosState extends State<Pontos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
